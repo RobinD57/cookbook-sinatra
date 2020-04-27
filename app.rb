020d65aba10da087cf2c392d0953a26d29508418
Robin Davids <robin.davidseth@gmail.com>
2020-04-27 14:10:28 +0200

    modified app.rb

--- app.rb
+++ app.rb
@@ -7,6 +7,18 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+set :bind, '0.0.0.0'
+
 get '/' do
-  'Hello Test!'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
